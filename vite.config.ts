fcff49a900f91846bc0e0db504c75821d8a3d459
Marc Lehane <marc.lehane@kinetechcloud.com>
2026-03-09 22:47:10 -0400

    fix allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   preview: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
     host: '0.0.0.0',
-    allowedHosts: ['all'],
+    allowedHosts: true,
   }
 })
